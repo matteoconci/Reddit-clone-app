ac4161efabbc724f7267257e709717a43bfdb279
Matteo Conci <matteo.conci54@hotmail.it>
2025-03-20 13:32:21 +0100

    Adjust some styling and add the comment underneath the post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from './Post.module.css';
+import Comment from "../Comment/Comment";
 
 function timeSinceDate(dateString) {
     const pastDate = new Date(dateString);
@@ -14,22 +15,41 @@ function timeSinceDate(dateString) {
     return `${diffInHours} h ago`.replace(".00", "");
 };
 
-const Post = ({id, title, content, media, author, avatar, subreddit, upvotes, comments, date}) => {
+const Post = ({id, title, content, media, author, avatar, subreddit, upvotes, postComments, date, comments}) => {
+    const filteredComments = comments.filter(comment => comment.postId === id);
+
     return (
         <>
             <div className={styles.postList}>
                 <div className={styles.topPost}>
-                    <img src={avatar} style={{width: '20px'}}/>
-                    <p>{author} · {timeSinceDate(date)}</p>
-                    <p className={styles.subreddit}>{subreddit}</p>
+                    <img className={styles.avatar} src={avatar} style={{width: '30px'}}/>
+                    <p>{author} · </p>
+                    <p className={styles.date}>{timeSinceDate(date)}</p>
+                    <p className={styles.subreddit}>r/{subreddit}</p>
                 </div>
                 <h3>{title}</h3>
                 <p className={styles.content}>{content}</p>
-                <img src={media}/>
+                <img className={styles.media} src={media}/>
                 <div className={styles.bottom}>
                     <p>{upvotes} upvotes</p>
-                    <p>{comments} comments</p>
+                    <p>{postComments} comments</p>
                 </div>
+            </div>
+            <div className={comments}>
+                {filteredComments.length > 0 ? (
+                        filteredComments.map((comment) => {
+                            return (
+                                <Comment 
+                                   key={comment.id}
+                                   author={comment.author.username}
+                                   avatar={comment.author.avatar}
+                                   content={comment.content}
+                                   upvotes={comment.upvotes}
+                                   date={comment.createdAt}
+                                />
+                            )
+                        })
+                ) : ( '' )}
             </div>
             <hr/>
         </>
