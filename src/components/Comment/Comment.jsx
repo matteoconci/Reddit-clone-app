ca0b92577ba8152cdb02ffa8fca325e1630c1503
Matteo Conci <matteo.conci54@hotmail.it>
2025-03-21 13:48:16 +0100

    Add code for show the comment only after click and upvotes system

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './Comment.module.css';
+import EmptyArrowUp from "../../assets/images/post/empty-up.svg";
+import EmptyArrowDown from "../../assets/images/post/empty-down.svg";
+import OrangeArrowUp from "../../assets/images/post/orange-up.svg";
+import OrangeArrowDown from "../../assets/images/post/orange-down.svg";
+
 
 function timeSinceDate(dateString) {
     const pastDate = new Date(dateString);
@@ -15,17 +20,42 @@ function timeSinceDate(dateString) {
 };
 
 const Comment = ({ comment }) => {
+    const [vote, setVote] = useState(null);
+    const [numVotes, setNumVotes] = useState(comment.upvotes);
+
+    const handleVote = (direction) => {
+        if (direction === vote) {
+            setVote(null)
+            setNumVotes(prev => direction === 'up' ? prev - 1 : prev + 1)
+        } else {
+            let change = 0;
+
+            if (vote === null) {
+                change = direction === 'up' ? 1 : -1;
+                } else if (vote === 'up' && direction === 'down') {
+                change = -2;
+                } else if (vote === 'down' && direction === 'up') {
+                change = 2;
+                };
+            
+            setVote(direction);
+            setNumVotes(prev => prev + change);
+        };
+    };
+    
     return (
         <>
             <div className={styles.commentList}>
                 <div className={styles.topComment}>
-                    <img src={comment.author.avatar} style={{width: '30px'}}/>
+                    <img className={styles.avatar} src={comment.author.avatar} style={{width: '30px'}}/>
                     <p>{comment.author.username} · </p>
                     <p className={styles.date}>{timeSinceDate(comment.createdAt)}</p>
                 </div>
                 <p className={styles.content}>{comment.content}</p>
                 <div className={styles.bottom}>
-                    <p>{comment.upvotes} upvotes</p>
+                    <img className={styles.arrow} src={vote === 'up' ? OrangeArrowUp : EmptyArrowUp} onClick={() => handleVote('up')} alt="arrow"/>
+                    <p>{numVotes}</p>
+                    <img className={styles.arrow} src={vote === 'down' ? OrangeArrowDown : EmptyArrowDown} onClick={() => handleVote('down')} alt="arrow"/>
                 </div>
             </div>
         </>
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './Post.module.css';
 import Comment from "../Comment/Comment";
+import EmptyArrowUp from "../../assets/images/post/empty-up.svg";
+import EmptyArrowDown from "../../assets/images/post/empty-down.svg";
+import OrangeArrowUp from "../../assets/images/post/orange-up.svg";
+import OrangeArrowDown from "../../assets/images/post/orange-down.svg";
 
 function timeSinceDate(dateString) {
     const pastDate = new Date(dateString);
@@ -16,6 +20,34 @@ function timeSinceDate(dateString) {
 };
 
 const Post = ({ post, comments }) => {
+    const [commentShown, setCommentShown] = useState(false);
+    const [vote, setVote] = useState(null);
+    const [numVotes, setNumVotes] = useState(post.upvotes);
+
+    const handleComment = () => {
+        setCommentShown(!commentShown);
+    };
+
+    const handleVote = (direction) => {
+        if (direction === vote) {
+            setVote(null)
+            setNumVotes(prev => direction === 'up' ? prev - 1 : prev + 1)
+        } else {
+            let change = 0;
+
+            if (vote === null) {
+                change = direction === 'up' ? 1 : -1;
+              } else if (vote === 'up' && direction === 'down') {
+                change = -2;
+              } else if (vote === 'down' && direction === 'up') {
+                change = 2;
+              };
+            
+            setVote(direction);
+            setNumVotes(prev => prev + change);
+        };
+    };
+
     const filteredComments = comments.filter(comment => comment.postId === post.id);
 
     return (
@@ -31,21 +63,23 @@ const Post = ({ post, comments }) => {
                 <p className={styles.content}>{post.content}</p>
                 <img className={styles.media} src={post.media}/>
                 <div className={styles.bottom}>
-                    <p>{post.upvotes} upvotes</p>
-                    <p>{post.comments} comments</p>
+                    <div className={styles.upvotes}>
+                        <img className={styles.arrow} src={vote === 'up' ? OrangeArrowUp : EmptyArrowUp} onClick={() => handleVote('up')} alt="arrow"/>
+                        <p>{numVotes}</p>
+                        <img className={styles.arrow} src={vote === 'down' ? OrangeArrowDown : EmptyArrowDown} onClick={() => handleVote('down')} alt="arrow"/>
+                    </div>
+                    <p style={{ cursor: 'pointer' }} onClick={handleComment}>{post.comments} comments</p>
                 </div>
             </div>
             <div className={comments}>
-                {filteredComments.length > 0 ? (
-                        filteredComments.map((comment) => {
-                            return (
-                                <Comment 
-                                   key={comment.id}
-                                   comment={comment}
-                                />
-                            )
-                        })
-                ) : ( '' )}
+                {commentShown && filteredComments.length > 0 && (
+                    filteredComments.map((comment) => (
+                        <Comment 
+                        key={comment.id}
+                        comment={comment}
+                        />   
+                    ))
+                )}
             </div>
             <hr/>
         </>
